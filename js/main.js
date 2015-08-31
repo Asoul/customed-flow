d16130d2c6529c9a1f580a05a32223be478fb74e
Asoul <azx754@gmail.com>
2015-08-31 13:32:27 +0800

    add pins and css layout

--- js/main.js
+++ js/main.js
@@ -1,30 +1,52 @@
+var follow_list = [
+  '136845026417486',//柯文哲
+  '481065478629613',//The News Lens 關鍵評論網
+]
 
-DisplayPosts = function(urls) {
-  console.log(urls)
-  var elem = document.querySelector("#posts")
-  elem.innerHTML = urls.map(function(url){
-    return "<img src='" + url + "'>"
+DisplayPosts = function(posts) {
+  var elem = document.querySelector("#columns")
+  elem.innerHTML += posts.map(function(post){
+    return "<div class='pin'>\
+              <img src='" + post.full_picture + "'>\
+              <p>" + post.message + "</p>\
+              <p>" + post.created_time + "</p>\
+            </div>"
   }).join('\n')
+}
+
+ValidPictureFilter = function(posts) {
+  return posts.filter(function(post){
+    return /^https/.exec(post.full_picture)
+  })
 }
 
 ExtractPictureURLs = function(response) {
   return response.map(function(obj){
-    return obj.picture
+    return obj.full_picture
   }).filter(function(url){
     return /^https/.exec(url)
   })
 }
 
 yo = function() {
-  (new FBApi).callKP()
-    .then(ExtractPictureURLs)
-    .then(DisplayPosts)
+  for (var i in follow_list) {
+    (new FBApi).queryPage(follow_list[i])
+    .then(ValidPictureFilter)
+    .then(DisplayPosts)  
+  }
 }
 
 function FBApi (){
+  this.queryPage = function(page) {
+    return new Promise(function(resolve, reject) {
+      FB.api(page + '/posts?fields=message,id,full_picture,created_time', function(response) {
+        resolve(response.data)
+      })
+    })
+  }
   this.callKP = function() {
     return new Promise(function(resolve, reject) {
-      FB.api('136845026417486/posts?fields=message,id,picture,created_time', function(response) {
+      FB.api('136845026417486/posts?fields=message,id,full_picture,created_time', function(response) {
         resolve(response.data)
       })
     })
